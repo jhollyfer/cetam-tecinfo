ea467e82ca6deb514b67f5bf0f0c2c57dfbaeb4c
Marcos Rodrigues <jhollyfer.fr@gmail.com>
2025-07-24 19:23:02 -0500

    initial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int idade;
+
+  cout << "Digite sua idade: ";
+  cin >> idade;
+
+  cout << "Seja bem-vindo(a)!" << endl;
+
+  return 0;
+}
